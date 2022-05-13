d7d3ef6e6bdd74aabd7b332b7aa2b24fc54de391
Vishwas Singh <031vishwas@gmail.com>
2022-05-13 20:03:18 +0530

    added favicon

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,16 @@ import About from "../components/About"
 import Skills from "../components/Skills"
 import Projects from "../components/Projects"
 import Contact from "../components/Contact"
+import Head from 'next/head'
+
 
 const Home = () => {
   return (
       <div>
+          <Head>
+            <title>Vishwas031</title>
+            <link rel="icon" href="/logo.jpg" />
+          </Head>
           <Navv/>
           <Home1 id='Home'/>
           <About id='About'/>
